37854500fa195d48ac552e517deaddf0c33ef356
dlw49 <46452923+dlw49@users.noreply.github.com>
2019-10-17 10:26:14 -0500

    Update list_test.cpp

--- basicllInorderEmpty/test/list_test.cpp
+++ basicllInorderEmpty/test/list_test.cpp
@@ -8,12 +8,12 @@ TEST_CASE("Test insert and print")
 {
 	List scores;
 	scores.InsertInOrder(12);
-	std::string oneitem = scores.getPrintableList();
+	std::string oneitem = scores.GetPrintableList();
 	REQUIRE(oneitem == "12 -> nullptr");
 	
 	scores.InsertInOrder(5);
 	scores.InsertInOrder(24235);
 	scores.InsertInOrder(-1);
-	std::string multi = scores.getPrintableList();
+	std::string multi = scores.GetPrintableList();
 	REQUIRE(multi = "-1 -> 5 -> 12 -> 24235 -> nullptr");
 }
